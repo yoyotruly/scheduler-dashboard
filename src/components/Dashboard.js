ddca75db4c8931fc96e097be2dd2a6b072878162
Yoyo Yang <yoyo.truly@gmail.com>
2022-08-15 20:11:58 -0400

    Import loading component to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 
+import Loading from "./Loading";
+
 import classnames from "classnames";
 
 class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+    };
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
